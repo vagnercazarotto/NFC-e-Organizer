87e2422a4b462fef02d2a4ae7903642c167a49b9
Vagner Cazarotto <vagnercazarotto@gmail.com>
2015-11-19 16:12:26 -0200

    New version for sincronize last two month

--- transfile.py
+++ transfile.py
@@ -28,8 +28,6 @@ ftp.login(user=userName,passwd=password)
 ## receive a list of directiories 
 docList = ftp.nlst()
 
-
-
 ### Block verify - mkdir and got to folder 
 ### create a folder name from actual date
 folderName = datetime.datetime.now().strftime("%Y"+"_"+"%m") + "_loja_" + str(storeNumber)
@@ -58,6 +56,53 @@ for x in itemList:
 	fileCreation = os.path.getctime(xmlFolder + x)
 	initialDate = datetime.datetime.fromtimestamp(fileCreation).strftime("%Y"+"_"+"%m")
 	if dateNow == initialDate:
+		if x not in fileList:
+			print "Enviando Arquivo!!"
+			print str(xmlFolder + x)
+			ftp.storbinary('STOR ' + str(x) , open(str(xmlFolder + x),'rb'))
+
+
+
+
+#### Verify last month
+
+## now go to root folter 
+ftp.cwd("/")
+
+## receive a list of directiories 
+docList = ftp.nlst()
+
+month = datetime.datetime.now().month-1
+year = datetime.datetime.now().year
+lastMonth = str(year) + "_" + str(month) 
+folderName = str(lastMonth) + "_loja_" + str(storeNumber)
+
+for x in docList:
+	if folderName == x:
+		cont = 1
+		break
+	else:
+	 	cont = 0
+if cont == 0:
+	ftp.mkd(folderName)
+for x in docList:
+	if folderName == x:
+		ftp.cwd(folderName)
+		break
+####
+
+## receive a list of directiories 
+fileList = ftp.nlst()
+
+
+### control send files
+itemList = os.listdir(xmlFolder)
+month = datetime.datetime.now().month-1
+year = datetime.datetime.now().year
+for x in itemList:
+	fileCreation = os.path.getctime(xmlFolder + x)
+	initialDate = datetime.datetime.fromtimestamp(fileCreation).strftime("%Y"+"_"+"%m")
+	if lastMonth == initialDate:
 		if x not in fileList:
 			print "Enviando Arquivo!!"
 			print str(xmlFolder + x)
